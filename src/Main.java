c1ed3d5f0dc8be0c5e93c7ade465b94aa3e9b9dc
drwn-srrws <afoninayou@gmail.com>
2022-10-24 09:16:38 +0300

    name

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        char ex3 = '\u263A';
+        char ex2_1 = '\u2639';
+        String ex2_2 = "\uD83D\uDE16";
+        String ex2_3 = "\ud83d\ude0d";
+        System.out.println("3 Смайлика:  " + ex2_1 + "  " +ex2_2 + "  " + ex2_3 );
+        System.out.println("Всім привіт" + ex3);
+
+    }
+}
